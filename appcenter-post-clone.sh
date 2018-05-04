faa32bab45fb7abbd792182ec256e91b64b19303
Yury Nevalenny <v-yuneva@microsoft.com>
2018-05-04 16:16:34 +0300

    fix

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -13,7 +13,8 @@ unzip -q -o -d "scripts" "appcenter-build-assets-latest.zip"
 ls -l -a "scripts"
 
 cd scripts
-sed -i.bak 's/\(tr -d .*\)/\1 \&\& echo "##vso[task.setvariable variable=APPLE_PROV_PROFILE_UUID]$UUID"/g' determine-signing-style-and-pods-update.sh
+sed -i.bak 's/\(tr -d .*\)/\1 \&\& echo \"##vso[task.setvariable variable=APPLE_PROV_PROFILE_UUID]$UUID\"/g' determine-signing-style-and-pods-update.sh
+cat determine-signing-style-and-pods-update.sh
 
 cd lib-build-xcproject-analyzer
 npm i yargs
